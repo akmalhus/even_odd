8c012ba844372ae33d2e281eb8cf7136f9e41857
akmalhus <95337227+akmalhus@users.noreply.github.com>
2021-12-03 10:28:20 +0530

    Add files via upload

---
+++ even_odd.java
@@ -0,0 +1,16 @@
+package basicifelse;
+
+public class even_odd {
+	public static void main(String[] args) {
+		int a=13;
+		if(a%2==0)
+		{
+			System.out.println("a is even");
+		}
+		else
+		{
+			System.out.println("a is odd");
+		}
+	}
+
+}
